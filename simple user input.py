200bb20db213e431824d3ca7a6429048a4e4ba68
Leo Magatao Rayos <rayosleojr@gmail.com>
2026-03-10 14:42:29 +0800

    Add files via upload

---
+++ simple user input.py
@@ -0,0 +1,14 @@
+# Simple User Input Program
+
+print("=== USER INFORMATION ===")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+course = input("Enter your course: ")
+
+print("\n=== INFORMATION ===")
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+
+print("\nThank you for providing your information!")
